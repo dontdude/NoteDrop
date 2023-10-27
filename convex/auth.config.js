a191cf8d3bdd029d43b19c0b20989db260945e3b
Chandan Mishra <imchandan1947@gmail.com>
2023-10-27 16:58:18 +0530

    convex bug fix

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://trusty-cowbird-32.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
